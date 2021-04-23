241120bcff3effa63fa5ad689ef363a4b55523fa
Anil Upadhyay <anil.upadhyay@daimler.com>
2021-04-23 11:36:44 +0530

    update swift version

--- LearningSwiftUIUITests/LearningSwiftUIUITests.swift
+++ LearningSwiftUIUITests/LearningSwiftUIUITests.swift
@@ -35,7 +35,7 @@ class LearningSwiftUIUITests: XCTestCase {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
+               // XCUIApplication().launch()
             }
         }
     }
